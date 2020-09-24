a8c24dbbf1b421571d34d590acab50809f000fd1
jason b <jason-19@hotmail.fr>
2020-09-24 12:33:54 +0200

    first push callback_additions.js

--- Cours/part3_async/exos/callback_additions.js
+++ Cours/part3_async/exos/callback_additions.js
@@ -1,7 +1,9 @@
-const add = (number, callback) => {
+const add = (number, callback, error) => {
     setTimeout(() => {
-        callback(number, () => console.log("ce n'est pas un nombre"))
+        isNaN(number) ? error() : callback(number);
+        //callback(number, () => console.log("ce n'est pas un nombre"))
     }, 1000);
 }
 
-const test = add(1, (number, error) => isNaN(number) ? error() : console.log(number + 2))
+const test = add(1, (number) => console.log(number + 2), () => console.log("not a number"))
+// const test = add(1, (number, error) => isNaN(number) ? error() : console.log(number + 2))
